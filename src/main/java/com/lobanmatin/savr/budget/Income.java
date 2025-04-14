1639a2e5ffb7612becd3346dcf7dde04ad6ca22d
Loban Matin <loban.matin@gmail.com>
2025-04-14 14:35:10 +1000

    create budgeting classes

---
+++ src/main/java/com/lobanmatin/savr/budget/Income.java
@@ -0,0 +1,4 @@
+package com.lobanmatin.savr.budget;
+
+public class Income {
+}
---
+++ src/main/java/com/lobanmatin/savr/budget/User.java
@@ -0,0 +1,10 @@
+package com.lobanmatin.savr.budget;
+
+public class User {
+    private Expense expense;
+    private Income income;
+    private Debt debt;
+    private Investments investments;
+    private Savings savings;
+
+}
