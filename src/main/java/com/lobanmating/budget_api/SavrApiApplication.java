1f0a6413007d9e73a3765086b0e1e10502e89322
Loban Matin <loban.matin@gmail.com>
2025-06-30 13:29:42 +1000

    Initial Spring Boot project setup

---
+++ src/main/java/com/lobanmating/budget_api/SavrApiApplication.java
@@ -0,0 +1,13 @@
+package com.lobanmating.budget_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobanmating/budget_api/SavrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobanmating.budget_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
