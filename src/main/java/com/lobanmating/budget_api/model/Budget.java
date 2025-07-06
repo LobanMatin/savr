9e3578b244bdf4bc17169f6316f809b2f487182c
Loban Matin <loban.matin@gmail.com>
2025-07-06 09:41:08 +0930

    added budget functionality with flyway database schema management

---
+++ src/main/java/com/lobanmating/budget_api/model/Budget.java
@@ -0,0 +1,38 @@
+package com.lobanmating.budget_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Data
+@Table(name = "budgets")
+public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private BigDecimal totalLimit;
+
+
+    // TODO: add functionality for multiple income streams
+    @Column(nullable = false)
+    private BigDecimal totalIncome;
+
+    @ElementCollection
+    @CollectionTable(name = "budget_category_limits", joinColumns = @JoinColumn(name = "budget_id"))
+    @MapKeyColumn(name = "category")
+    @Column(name = "category_limit")
+    private Map<String, BigDecimal> categoryLimits = new HashMap<>();
+}
