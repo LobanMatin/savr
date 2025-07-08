094f0eb28aa73ebc08b92f4ea56ec9feeb6b9c08
Loban Matin <loban.matin@gmail.com>
2025-07-08 20:58:44 +0930

    Completed writing swagger documentation with error handling and readme

--- src/main/java/com/lobanmatin/budget_api/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.lobanmatin.budget_api.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String email) {
-        super("The following email is already registered to an account: " + email);
-    }
-}
--- src/main/java/com/lobanmatin/budget_api/service/UserService.java
+++ src/main/java/com/lobanmatin/budget_api/service/UserService.java
@@ -1,10 +1,11 @@
 package com.lobanmatin.budget_api.service;
 import com.lobanmatin.budget_api.dto.UserRequest;
-import com.lobanmatin.budget_api.exception.EmailAlreadyExistsException;
-import com.lobanmatin.budget_api.exception.UserNotFoundException;
+import com.lobanmatin.budget_api.exception.ResourceNotFoundException;
 import com.lobanmatin.budget_api.model.User;
 import com.lobanmatin.budget_api.repository.UserRepository;
 import com.lobanmatin.budget_api.model.Role;
+import jakarta.transaction.Transactional;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.stereotype.Service;
@@ -23,7 +24,7 @@ public class UserService {
 
     public void createUser(UserRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new EmailAlreadyExistsException(request.getEmail());
+            throw new DataIntegrityViolationException("Email already exists for this user, creation unsuccessful.");
         }
 
         String encodedPassword = passwordEncoder.encode(request.getPassword());
@@ -42,11 +43,12 @@ public class UserService {
 
     public User getUserById(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(id));
+                .orElseThrow(() -> new ResourceNotFoundException("User not found."));
     }
 
+    @Transactional
     public void deleteAllUsers() {
-        userRepository.deleteAll();
+        userRepository.deleteAllByRoleUser();
     }
 }
 
